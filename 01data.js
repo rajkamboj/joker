02f98a14cbed5d53efa1c2b4fce34b39a10f2389
rajkamboj <i.rajkamboj@gmail.com>
2024-02-19 18:10:46 +0530

    update

---
+++ 01data.js
@@ -0,0 +1,22 @@
+let score = null
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" = 33
+// "45" + "10" = "4510"
+// "33abc" = NaN
+// parseInt("33abc") = 33
+
+let isLoggedIn = "ophuhu"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let rohit = 5
+let stringNumber = String(rohit)
+console.log(rohit);
+console.log(typeof stringNumber);
---
+++ 01vairiable.js
@@ -0,0 +1,11 @@
+const accountId = 151653
+let accountEmail = "test@gmail.com"
+let accountPassword = "123456789Aa."
+let accountCity =  "London"
+/*
+* Welcome to the world of NodeJS! This is a basic example on how you can use long comment
+*/
+// this use for single line comment
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
